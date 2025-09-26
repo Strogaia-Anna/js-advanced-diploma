27579703b086d991e2ab0b9067ae3f7a420c05ef
Strogaia-Anna <strogaia.ania@gmail.com>
2025-09-26 19:53:38 +0300

    Delete src directory

--- src/js/__tests__/Character.test.js
+++
@@ -1,8 +0,0 @@
-import Character from "../Character";
-
-test('should be mistake', () => {
-  expect(() => {
-    new Character(3);
-  }).toThrow('Character must be called without new');
-});
-
--- src/js/__tests__/utils.test.js
+++
@@ -1,9 +0,0 @@
-import { genTooltipMessage } from "../utils"
-import { Bowman } from "../characters/Bowman";
-
-test('should generate Tooltip message', () => {
-  const char = new Bowman(1);
-    const result = genTooltipMessage(char); 
-    
-  expect(result).toBe(`🎖${char.level}⚔${char.attack}🛡${char.defence}❤${char.health}`);
-});
